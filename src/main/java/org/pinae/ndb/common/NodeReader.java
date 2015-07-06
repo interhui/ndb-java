4465aff53f6c4275b64252e117622097c5cc78e7
Yugeng Hui <interhuiyg@163.com>
2015-07-06 18:45:44 +0800

    fix NodeReader bugs

--- src/main/java/org/pinae/ndb/common/NodeReader.java
+++ src/main/java/org/pinae/ndb/common/NodeReader.java
@@ -20,32 +20,34 @@ import org.apache.commons.lang3.StringUtils;
  *
  */
 public class NodeReader {
-	
-	private int linenum = 0; //配置行号
-	
+
+	private int linenum = 0; // 配置行号
+
 	/**
 	 * 载入解析ndb文件内容
 	 * 
-	 * @param filename 文件名
+	 * @param filename
+	 *            文件名
 	 * 
 	 * @return 解析后的ndb信息
 	 * 
-	 * @throws IOException 异常处理
+	 * @throws IOException
+	 *             异常处理
 	 */
-	public Map<String, Object> read(String filename) throws IOException{
+	public Map<String, Object> read(String filename) throws IOException {
 		List<String> fileContentList = new ArrayList<String>();
 		BufferedReader br = new BufferedReader(new FileReader(filename));
 		String line = "";
 		while ((line = br.readLine()) != null) {
-			 fileContentList.add(line);
+			fileContentList.add(line);
 		}
 		br.close();
-		
+
 		Map<String, Object> ndb = parse(fileContentList);
-		
+
 		return ndb;
 	}
-	
+
 	/*
 	 * 解析为Map
 	 * 
@@ -55,69 +57,67 @@ public class NodeReader {
 	 * 
 	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private Map<String, Object> parse(List<String> fileContentList){
+	private Map<String, Object> parse(List<String> fileContentList) {
 		Map<String, Object> ndb = new HashMap<String, Object>();
-		
-		while(linenum < fileContentList.size()){
-			
+
+		while (linenum < fileContentList.size()) {
+
 			String line = fileContentList.get(linenum).trim();
-			
+
 			linenum++;
-			
-			if(line==null || line.equals("") || StringUtils.startsWith(line, "#")){
+
+			if (line == null || line.equals("") || StringUtils.startsWith(line, "#")) {
 				continue;
 			}
 
-			if(line.endsWith("{")){
-				String nodeName = line.substring(0, line.indexOf("{"));
+			if (line.endsWith("{")) {
+				String nodeName = line.substring(0, line.indexOf("{")).trim();
 				Map<String, Object> nodeValue = parse(fileContentList);
-				
+
 				Object node = ndb.get(nodeName);
-				if(node==null){
+				if (node == null) {
 					ndb.put(nodeName, nodeValue);
-				}else{
+				} else {
 					List nodeList = null;
-					if(node instanceof List){
-						nodeList = (List)node;
-					}else{
+					if (node instanceof List) {
+						nodeList = (List) node;
+					} else {
 						nodeList = new ArrayList();
 						nodeList.add(node);
 					}
 					nodeList.add(nodeValue);
 					ndb.put(nodeName, nodeList);
 				}
-				
-				
-			}else if(line.endsWith("}")){
+
+			} else if (line.endsWith("}")) {
 				return ndb;
-			}else {
+			} else {
 				String items[] = new String[2];
-				if(line.indexOf(":")>0){
-					items[0] = line.substring(0, line.indexOf(":"));
-					items[1] = line.substring(line.indexOf(":") + 1, line.length());
+				if (line.indexOf(":") > 0) {
+					items[0] = line.substring(0, line.indexOf(":")).trim();
+					items[1] = line.substring(line.indexOf(":") + 1, line.length()).trim();
 				}
-				
+
 				Object itemValue = ndb.get(items[0]);
-				if (itemValue != null){
+				if (itemValue != null) {
 					List itemValueList = null;
-					if (itemValue instanceof List){
-						itemValueList = (List)itemValue;
-					}else{
+					if (itemValue instanceof List) {
+						itemValueList = (List) itemValue;
+					} else {
 						itemValueList = new ArrayList();
 						itemValueList.add(itemValue);
 					}
 					itemValueList.add(items[1]);
-					
+
 					ndb.put(items[0], itemValueList);
-				}else{
+				} else {
 					ndb.put(items[0], items[1]);
 				}
-				
+
 			}
-			
-			
+
 		}
-		
+
 		return ndb;
 	}
 }
