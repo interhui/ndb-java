773755802ca329ae46e839e6f651e1462a522782
Yugeng Hui <interhuiyg@163.com>
2015-11-27 23:01:03 +0800

    Add join To MapHelper and Update toStringMap / toObjectMap

--- src/main/java/org/pinae/ndb/common/MapHelper.java
+++ src/main/java/org/pinae/ndb/common/MapHelper.java
@@ -1,6 +1,7 @@
 package org.pinae.ndb.common;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -12,7 +13,7 @@ import java.util.Set;
  *
  */
 public class MapHelper {
-	
+
 	/**
 	 * Map对象拷贝
 	 * 
@@ -21,16 +22,16 @@ public class MapHelper {
 	 * 
 	 * @return 拷贝后的Map对象
 	 */
-	public static Map<?, ?> copy(Map<?, ?> src, Map<Object, Object> propertyRef) {
+	public static Map<?, ?> copy(Map<?, ?> src, Map<?, ?> propertyRef) {
 		Map<Object, Object> dst = new HashMap<Object, Object>();
-		
+
 		Set<?> entrySet = src.entrySet();
 		for (Object object : entrySet) {
 			if (object != null && object instanceof Entry<?, ?>) {
-				Entry<?, ?> entry = (Entry<?, ?>)object;
+				Entry<?, ?> entry = (Entry<?, ?>) object;
 				Object key = entry.getKey();
 				Object value = entry.getValue();
-				
+
 				if (key != null) {
 					Object refKey = propertyRef.get(key);
 					if (refKey != null) {
@@ -39,11 +40,11 @@ public class MapHelper {
 				}
 			}
 		}
-		
+
 		return dst;
-		
+
 	}
-	
+
 	/**
 	 * 将数组输出为Map，其中键与值在位置上一一对应
 	 * 
@@ -52,22 +53,22 @@ public class MapHelper {
 	 * 
 	 * @return 生成的Map对象
 	 */
-	public static Map<String, String> toMap(String keys[], String values[]) {
-		Map<String, String> dst = new HashMap<String, String>();
-		
+	public static Map<Object, Object> toMap(Object keys[], Object values[]) {
+		Map<Object, Object> dst = new HashMap<Object, Object>();
+
 		if (keys != null && values != null && keys.length <= values.length) {
-			for (int i = 0; i < keys.length; i++ ) {
-				String key = keys[i];
-				String value = values[i];
+			for (int i = 0; i < keys.length; i++) {
+				Object key = keys[i];
+				Object value = values[i];
 				if (key != null) {
 					dst.put(key, value);
 				}
 			}
 		}
-		
+
 		return dst;
 	}
-	
+
 	/**
 	 * 将数组输出为Map
 	 * 
@@ -75,15 +76,15 @@ public class MapHelper {
 	 * 
 	 * @return 生成的Map对象
 	 */
-	public static Map<String, String> toMap(String[][] valuePairs) {
-		Map<String, String> dst = new HashMap<String, String>();
+	public static Map<Object, Object> toMap(Object[][] valuePairs) {
+		Map<Object, Object> dst = new HashMap<Object, Object>();
 		if (valuePairs != null) {
 			for (int i = 0; i < valuePairs.length; i++) {
-				String[] valuePair = valuePairs[i];
+				Object[] valuePair = valuePairs[i];
 				if (valuePair != null && valuePair.length == 2) {
-					String key = valuePair[0];
-					String value = valuePair[1];
-					
+					Object key = valuePair[0];
+					Object value = valuePair[1];
+
 					if (key != null) {
 						dst.put(key, value);
 					}
@@ -92,7 +93,7 @@ public class MapHelper {
 		}
 		return dst;
 	}
-	
+
 	/**
 	 * Map格式转换: Object转换为String
 	 * 
@@ -100,16 +101,16 @@ public class MapHelper {
 	 * 
 	 * @return 转换后的Map
 	 */
-	public static Map<String, String> convertToStringMap(Map<?, ?> src) {
+	public static Map<String, String> toStringMap(Map<?, ?> src) {
 		Map<String, String> dst = new HashMap<String, String>();
-		
+
 		Set<?> entrySet = src.entrySet();
 		for (Object object : entrySet) {
 			if (object != null && object instanceof Entry<?, ?>) {
-				Entry<?, ?> entry = (Entry<?, ?>)object;
+				Entry<?, ?> entry = (Entry<?, ?>) object;
 				Object key = entry.getKey();
 				Object value = entry.getValue();
-				
+
 				if (key != null) {
 					if (value == null) {
 						value = "";
@@ -118,10 +119,10 @@ public class MapHelper {
 				}
 			}
 		}
-		
+
 		return dst;
 	}
-	
+
 	/**
 	 * Map格式转换: String转换为Object
 	 * 
@@ -130,28 +131,27 @@ public class MapHelper {
 	 * @return 转换后Map
 	 */
 	@SuppressWarnings("unchecked")
-	public static Map<Object, Object> convertToObjectMap(Map<String, String> src) {
+	public static Map<Object, Object> toObjectMap(Map<String, String> src) {
 		Map<Object, Object> dst = new HashMap<Object, Object>();
-		
+
 		Set<?> entrySet = src.entrySet();
 		for (Object object : entrySet) {
 			if (object != null && object instanceof Entry<?, ?>) {
-				Entry<Object, Object> entry = (Entry<Object, Object>)object;
+				Entry<Object, Object> entry = (Entry<Object, Object>) object;
 				Object key = entry.getKey();
 				Object value = entry.getValue();
-				
+
 				if (key != null) {
 					dst.put(key, value);
 				}
 			}
 		}
-		
+
 		return dst;
 	}
-	
+
 	/**
-	 * Map转换为字符串
-	 * 根据format格式对Map中的值进行映射，format中需要替换的值使用{}进行包装
+	 * Map转换为字符串 根据format格式对Map中的值进行映射，format中需要替换的值使用{}进行包装
 	 * 
 	 * @param src 需要转换的Map
 	 * @param format 字符串格式，例如"{firstname} {lastname}"
@@ -174,5 +174,41 @@ public class MapHelper {
 		}
 		return format;
 	}
+
+	/**
+	 * 合并两个Map, 将源Map合并至目标Map
+	 * 
+	 * @param dst 目标Map
+	 * @param src 源Map
+	 */
+	@SuppressWarnings("unchecked")
+	public static void join(Map<?, ?> dst, Map<?, ?> src) {
+		
+		Map<Object, Object> dstMap = (Map<Object, Object>)dst;
+		Map<Object, Object> srcMap = (Map<Object, Object>)src;
+		
+		Set<Object> srcKeySet = srcMap.keySet();
+
+		for (Object srcKey : srcKeySet) {
+
+			Object srcValue = src.get(srcKey);
+			Object dstValue = dst.get(srcKey);
+
+			if (srcValue != null) {
+				if (dstValue != null) {
+					if (dstValue instanceof List) {
+						List<Object> valueList = (List<Object>) dstValue;
+						valueList.add(srcValue);
+						dstMap.put(srcKey, valueList);
+					} else {
+						dstMap.put(srcKey, srcValue);
+					}
+				} else {
+					dstMap.put(srcKey, srcValue);
+				}
+			}
+		}
+	}
 	
+
 }
