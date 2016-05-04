6cebb59f5242b8e466a2a1b4e0a7e8c2b72ab5f7
Yugeng Hui <interhuiyg@163.com>
2016-05-04 16:46:48 +0800

    Bug Fixes

--- src/main/java/org/pinae/ndb/operate/Cleaner.java
+++ src/main/java/org/pinae/ndb/operate/Cleaner.java
@@ -15,54 +15,54 @@ import org.apache.commons.lang3.StringUtils;
  *
  */
 public class Cleaner {
-	
+
 	/**
-	 * 清理ndb中的节点
-	 * 如果节点下面没有任何数据，则该节点会被清理
+	 * 清理ndb中的节点 如果节点下面没有任何数据，则该节点会被清理
 	 * 
 	 * @param ndb 需要清理的ndb节点
 	 * 
 	 * @return 清理后的ndb节点
 	 */
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public Map<String, Object> clean(Map<String, Object> ndb){
+	@SuppressWarnings({ "unchecked" })
+	public Map<String, Object> clean(Map<String, Object> ndb) {
 		Map<String, Object> ndbResult = new HashMap<String, Object>();
-		
+
 		Set<String> keySet = ndb.keySet();
-		
-		for (String key : keySet){
+
+		for (String key : keySet) {
 			Object value = ndb.get(key);
-			
-			if (value instanceof String){
-				ndbResult.put(key, value);
-			}else if (value instanceof List){
-				List<Object> list = (List<Object>)value;
-				
-				List<Object> _list = new ArrayList<Object>();
-				
-				for(Object item : list){
-					if (item instanceof String){
-						if (StringUtils.isNotBlank((String)item)){
-							_list.add(item);
+
+			if (value instanceof String) {
+				if (StringUtils.isNotBlank((String) value)) {
+					ndbResult.put(key, value);
+				}
+			} else if (value instanceof List) {
+				List<Object> list = (List<Object>) value;
+
+				List<Object> tmpList = new ArrayList<Object>();
+
+				for (Object item : list) {
+					if (item instanceof String) {
+						if (StringUtils.isNotBlank((String) item)) {
+							tmpList.add(item);
 						}
-					}else if (item instanceof Map){
-						Map map = (Map)item;
-						if (map.size() > 0) {
-							_list.add(clean((Map<String, Object>)item));
+					} else if (item instanceof Map) {
+						Map<String, Object> mapItem = (Map<String, Object>) item;
+						if (mapItem.size() > 0) {
+							tmpList.add(clean(mapItem));
 						}
 					}
 				}
-				if (_list.size() > 0) {
-					ndbResult.put(key, _list);
+				if (tmpList.size() > 0) {
+					ndbResult.put(key, tmpList);
 				}
-			}else if (value instanceof Map){
-				Map map = (Map)value;
-				if (map.size() == 0) {
+			} else if (value instanceof Map) {
+				Map<String, Object> mapItem = (Map<String, Object>) value;
+				if (mapItem.size() == 0) {
 					ndbResult.remove(key);
 				} else {
-					ndbResult.put(key, clean((Map<String, Object>)value));
+					ndbResult.put(key, clean(mapItem));
 				}
-
 			}
 		}
 		return ndbResult;
